4e849db4effb31c334f7baae2aecd028e6f6b806
ManasiD-cuelgoic <manasi.deshmukh@cuelogic.com>
2020-08-18 11:54:06 +0530

    add first file

---
+++ calculator.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+if __name__ == __"main"__:
+    print("Enter First Number")
+    x=int(input())
+    print("Enter Second Number")
+    y=int(input())
+    print("addition = ",add(x,y))
+
