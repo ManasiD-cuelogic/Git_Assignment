a60b16a86f84b0ed562326b6633ba7ad31b34ada
ManasiD-cuelgoic <manasi.deshmukh@cuelogic.com>
2020-08-18 17:02:15 +0530

    commit changes

--- calculator.py
+++ calculator.py
@@ -1,9 +1,12 @@
 def add(a,b):
     return a+b
+def multiply(a,b):
+    return a*b
 if __name__ == __"main"__:
     print("Enter First Number")
     x=int(input())
     print("Enter Second Number")
     y=int(input())
     print("addition = ",add(x,y))
+    print("multiplication = ",multiply(x,y))
 
